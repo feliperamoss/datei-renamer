d76e0436341cc375005aab0fbc25d9ac55d416b5
Felipe Pedroso Ramos <felipe.pedroso-ramos@odt.net>
2023-06-15 10:12:39 +0200

    inital commit for the console app

---
+++ console-app-renamer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console_app_renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
