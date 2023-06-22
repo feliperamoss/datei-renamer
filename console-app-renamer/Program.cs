cfbbe6d0a68d0c0452a2f371b80b0abf34852905
SouliyoCam <116083749+SouliyoCam@users.noreply.github.com>
2023-06-22 10:12:06 +0200

    TestCommit

--- console-app-renamer/Program.cs
+++ console-app-renamer/Program.cs
@@ -6,7 +6,30 @@ namespace console_app_renamer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Souliyo function
+            //static void Umbenennen()
+            //{
+            //    string oldFilePath = "C:\\Users\\Souli\\Documents\\Berufsschule\\Software-Projekt\\Test-Ordner\\Rename-Folder-Test\\AK 2__Themen E1  Wi+GK.docx";
+            //    string newFilePath = "C:\\Users\\Souli\\Documents\\Berufsschule\\Software-Projekt\\Test-Ordner\\Rename-Folder-Test\\KA 2__Themen E1  Wi+GK.docx";
+
+            //    File.Move(oldFilePath, newFilePath);
+            //}
+
+            string FilePath;
+            string oldFile;
+            string newFile;
+
+            // Optional falls nur bestimmte Datei-Format umbenannt werden will.
+            string[] format = new string[7] { ".img", ".png", ".docx", ".jpg", ".txt" };
+
+            FilePath = selectFolderTextBox.Text;
+            oldFile = originalNameTextBox.Text;
+            newFile = newNameTextBox.Text;
+
+            string oldFilePath = FilePath + "\\" + oldFile;
+            string newFilePath = FilePath + "\\" + newFile;
+
+            File.Move(oldFilePath, newFilePath);
         }
     }
 }
